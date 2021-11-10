55af3ffd9e5f5d5dc446e530687ddaa10bb30b3f
DineshDp27 <dineshpaul98@gmail.com>
2021-11-10 15:29:59 +0530

    Sample

---
+++ src/org/git/GitImport.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class GitImport {
+	
+	private void empName() {
+		System.out.println("Dinesh");
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	
+		GitImport g = new GitImport();
+		g.empName();
+	}
+	
+	
+
+}
